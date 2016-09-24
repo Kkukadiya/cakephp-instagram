d0319eb93c7f8cd494b9a637a0294a2eb4c2914b
Narendra Vaghela <narendravaghela4389@gmail.com>
2016-09-24 17:02:28 +0530

    Update exception description

--- src/Utility/Exception/MissingCredentialsException.php
+++ src/Utility/Exception/MissingCredentialsException.php
@@ -5,8 +5,8 @@ namespace Instagram\Utility\Exception;
 use RuntimeException;
 
 /**
- * Exception class for Xml.  This exception will be thrown from Xml when it
- * encounters an error.
+ * MissingCredentialsException class.  This exception will be thrown from
+ * Instagram\Utility\InstagramClient when Client ID or Secret is missing.
  */
 class MissingCredentialsException extends RuntimeException
 {
